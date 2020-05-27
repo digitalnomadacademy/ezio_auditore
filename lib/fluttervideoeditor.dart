50318e670e3b05744e45168bb53d35db5a6acc35
mohsin2596 <mohsin2596@gmail.com>
2020-05-27 19:42:39 +0500

    Some initial testing for framework

--- lib/fluttervideoeditor.dart
+++ lib/fluttervideoeditor.dart
@@ -1,7 +1,43 @@
 library fluttervideoeditor;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'package:fluttervideoeditor/codecs.dart';
+import 'package:path_provider/path_provider.dart';
+
+/// VideoEditor class which will be used by our VideoViewer as well.
+class VideoEditor {
+  final FlutterFFmpeg _flutterFFmpeg = new FlutterFFmpeg();
+
+  /// Returns codec config for video output, should be private
+  /// as used internally only
+  CodecConfig _getCodecConfig(Codec codec) {
+    switch (codec) {
+      case Codec.x264:
+        return CodecConfig(libraryName: 'libx264', encodingOptions: '');
+
+      case Codec.x265:
+        return CodecConfig(
+            libraryName: 'libx265', encodingOptions: '-crf 28 -preset fast ');
+    }
+  }
+
+  /// Function to encode a given video file with the required codec
+  Future<int> encodeVideo(String videoPath, Codec codec) async {
+    //Get temp file path
+    var tempDir = await getTemporaryDirectory();
+    final tempPath = '${tempDir.path}temp.mp4';
+
+    final codecConfig = _getCodecConfig(codec);
+
+    final script = "-y -i " +
+        videoPath +
+        " -filter:v " +
+        codecConfig.encodingOptions +
+        "-c:v " +
+        codecConfig.libraryName +
+        " -r 30 " +
+        tempPath;
+
+    return _flutterFFmpeg.execute(script);
+  }
 }
