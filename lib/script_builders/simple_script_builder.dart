7949834a1dddc65c1f0f686d1d88135392031bee
Mohsin Ajmal <mohsin2596@gmail.com>
2020-08-04 00:34:20 +0500

    Refactored simple script builder

--- lib/script_builders/simple_script_builder.dart
+++ lib/script_builders/simple_script_builder.dart
@@ -2,6 +2,7 @@ import 'package:flutter_video_editor/codecs.dart';
 import 'package:flutter_video_editor/constants/presets.dart';
 import 'package:flutter_video_editor/filters/drawtext_filter.dart';
 import 'package:flutter_video_editor/script_builders/base_script_builder.dart';
+import 'package:flutter_video_editor/script_builders/drawtext_script_builder.dart';
 import 'package:flutter_video_editor/script_builders/watermark_script_builder.dart';
 import 'package:flutter_video_editor/filters/watermark_filter.dart';
 
@@ -46,17 +47,16 @@ class SimpleScriptBuilder implements BaseScriptBuilder {
             withFilter: false)
         .build();
 
-    /// For documentation regarding flags https://ffmpeg.org/ffmpeg.html#toc-Main-options
+    final textFilterScript =
+        DrawTextScriptBuilder(textFilters, fontPath).build();
 
-    // Todo: Separate scripts for cases 1: With watermark and text, 2: Text only
+    final filters = _filters(watermarkFilter, textFilterScript);
+
+    /// For documentation regarding flags https://ffmpeg.org/ffmpeg.html#toc-Main-options
     return "-y -i " +
         videoPath +
         " " +
-        watermarkFilter.input +
-        "-filter_complex \"[0:v][1:v]${watermarkFilter.complexFilter},drawtext=fontfile='$fontPath':fontsize=90:x=20:y=20:text='Testing':enable='between(t\\,1\\,2)',drawbox=enable='between(t\\,3\\,4)':y=ih-h:color=black:width=iw:height=350:t=fill,drawtext=fontfile='$fontPath':fontsize=90:fontcolor=white:x=(w-text_w)/2:y=h-th:text='OTHER TEXT LONG SUPER\n LONG LONG LONG':enable='between(t\\,3\\,4)'\" " +
-
-        // watermarkFilter.complexFilter +
-        //" " +
+        filters +
         _codecConfig.encodingOptions +
         " " +
         "-c:v " +
@@ -64,6 +64,17 @@ class SimpleScriptBuilder implements BaseScriptBuilder {
         " -r $outputRate " +
         outputPath;
   }
-}
 
-//"-filter_complex \"[0:v][1:v]${watermarkFilter.complexFilter},drawtext=fontfile='$fontPath':fontsize=90:x=20:y=20:text='Testing':enable='between(t\\,1\\,2)'\" " +
+  String _filters(WatermarkFiler watermarkFilter, String textFilterScript) {
+    if (textFilterScript.isEmpty) {
+      return watermarkFilter.input + watermarkFilter.complexFilter + " ";
+    } else if (watermarkFilter.input.isNotEmpty &&
+        textFilterScript.isNotEmpty) {
+      return watermarkFilter.input +
+          "-filter_complex \"[0:v][1:v]${watermarkFilter.complexFilter},$textFilterScript\" ";
+    } else {
+      // Only text filter needs to applied
+      return "-filter_complex \"[0:v][1:v]$textFilterScript\" ";
+    }
+  }
+}
